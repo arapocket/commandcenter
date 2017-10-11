35d4842f3b77a97a121fec54215e26cb10b0b9b5
arapocket <arakazaryan@me.com>
2017-10-10 20:58:36 -0700

    pass by flatulence reference;:

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -124,7 +124,7 @@ function createRoutes(map, iconsBase) {
 
     removeCheckpointButton.addEventListener('click', function (e) {
 
-        onRemoveCheckpoint(route, routeMarkers);
+        onRemoveCheckpoint(route, routeMarkers[routeMarkers.length-1]);
     });
 
 
@@ -170,12 +170,10 @@ function createRouteMarker(latLng, map, iconsBase, route, routeMarkers) {
 
 }
 
-function onRemoveCheckpoint(route, routeMarkers) {
-    console.log(routeMarkers);
+function onRemoveCheckpoint(route, lastRouteMarker) {
+    console.log(lastRouteMarker);
     route.getPath().pop();
-    if (routeMarkers.length > 0) {
-        routeMarkers[routeMarkers.length - 1].setMap(null);
-    }
+        lastRouteMarker.setMap(null);
 }
 
 
