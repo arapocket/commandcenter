d8db82156e21464521778b9e54edf30bc65a19ac
arapocket <arakazaryan@me.com>
2017-10-19 00:25:20 -0700

    unset

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -415,7 +415,7 @@ function onAutoRefresh(autoRefreshButtonOFF, autoRefreshButtonSIXTY, buttonValue
     if (currentButtonValue == buttonValues.OFF) {
         autoRefreshButtonOFF.style.display = "none";
         currentButtonValue = buttonValues.SIXTY
-        autoRefreshButtonSIXTY.style.display = "block";
+        autoRefreshButtonSIXTY.style.display = "unset";
     } else if (currentButtonValue == buttonValues.SIXTY) {
     }
 
