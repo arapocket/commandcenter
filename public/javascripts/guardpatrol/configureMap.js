be6371b2d37bbbdd67f623f24944cb0559fee27b
arapocket <arakazaryan@me.com>
2017-10-19 00:36:43 -0700

    1

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -415,7 +415,7 @@ function onAutoRefresh(autoRefreshButtonOFF, autoRefreshButtonSIXTY, buttonValue
     if (currentButtonValue == buttonValues.OFF) {
         autoRefreshButtonOFF.style.display = "none";
         currentButtonValue = buttonValues.SIXTY
-        autoRefreshButtonSIXTY.style.opacity = "1";
+        autoRefreshButtonSIXTY.style.opacity = 1;
     } else if (currentButtonValue == buttonValues.SIXTY) {
     }
 
