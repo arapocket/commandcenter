1c9f27e9a19b30ea038e38fafd6c5eeb3ff68211
arapocket <arakazaryan@me.com>
2017-10-22 23:28:56 -0700

    anchro is easier

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -100,8 +100,10 @@ function createIncidentMarkers(incidents, map, iconsBase) {
         <h6 style="color:#404040"> 
         ` + incidents[i].Description + `
         </h6>
-        <img src="https://s3-us-west-2.amazonaws.com/foxwatch/`
-            + incidents[i].IncidentID + `"height="100" width="100"  >`
+        <a href = "https://s3-us-west-2.amazonaws.com/foxwatch/" ` + incidents[i].IncidentID +
+    
+        `> <img src="https://s3-us-west-2.amazonaws.com/foxwatch/`
+            + incidents[i].IncidentID + `"height="100" width="100" href = ""  ></a>`
 
         var markerWindow = new google.maps.InfoWindow({
             content: windowString
