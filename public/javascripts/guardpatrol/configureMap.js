0b956ce8eb05b1a6ea1eeaa9fd77a740016a2e95
arapocket <arakazaryan@me.com>
2017-10-23 00:46:31 -0700

    asdasd:

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -425,8 +425,8 @@ function loadRoutesOnMap(checkpoints, map, iconsBase, route, routeMarkers) {
     route.setPath([]);
     for (i = 0 ; i < routeMarkers.length ; i++){
         routeMarkers[i].setMap(null);
+        console.log(routeMarkers[i];)
     }
-    routeMarkers = [];
     for (i = 0; i < checkpoints.length; i++) {
         var latLng = new google.maps.LatLng(checkpoints[i].lat, checkpoints[i].lng);
         route.getPath().push(latLng);
