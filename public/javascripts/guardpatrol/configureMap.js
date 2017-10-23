21f7e1743d70d0cd385b1389d57021e04b7aae16
arapocket <arakazaryan@me.com>
2017-10-22 22:15:10 -0700

    testing the modal

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -100,8 +100,29 @@ function createIncidentMarkers(incidents, map, iconsBase) {
         <h6 style="color:#404040"> 
         ` + incidents[i].Description +`
         </h6>
-        <img src="https://s3-us-west-2.amazonaws.com/foxwatch/`
+        <img data-toggle="modal" data-target="#exampleModal" src="https://s3-us-west-2.amazonaws.com/foxwatch/`
         + incidents[i].IncidentID +`"height="100" width="100" >`
+        +`
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        `
     
 
         
