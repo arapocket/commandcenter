763f638a7c1e4aa43e847dff17ec1c5e7feac3b8
arapocket <arakazaryan@me.com>
2017-10-22 23:06:41 -0700

    asdasd

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -101,28 +101,25 @@ function createIncidentMarkers(incidents, map, iconsBase) {
         ` + incidents[i].Description + `
         </h6>
         <img src="https://s3-us-west-2.amazonaws.com/foxwatch/`
-            + incidents[i].IncidentID + `"height="100" width="100" >`
-            + `
-    <!-- Trigger the Modal -->
-    <img id="myImg" src="https://s3-us-west-2.amazonaws.com/foxwatch/lqyv9xq8x" alt="Trolltunga, Norway" width="300" height="200">
-    
-    <!-- The Modal -->
-    <div id="myModal" class="modal">
-    
-      <!-- The Close Button -->
-      <span class="close">&times;</span>
-    
-      <!-- Modal Content (The Image) -->
-      <img class="modal-content" id="img01">
-    
-      <!-- Modal Caption (Image Text) -->
-      <div id="caption"></div>
-    </div>
-    `
+            + incidents[i].IncidentID + `"height="100" width="100"  >`+
+         `
+         <!-- Trigger the Modal -->
+         <img id="myImg" src="img_fjords.jpg" alt="Trolltunga, Norway" width="300" height="200" onclick= "imageClick()">
+         
+         <!-- The Modal -->
+         <div id="myModal" class="modal">
+         
+           <!-- The Close Button -->
+           <span class="close">&times;</span>
+         
+           <!-- Modal Content (The Image) -->
+           <img class="modal-content" id="img01">
+         
+           <!-- Modal Caption (Image Text) -->
+           <div id="caption"></div>
+         </div>
 
-
-
-
+         `   
 
 
         var markerWindow = new google.maps.InfoWindow({
@@ -505,5 +502,29 @@ function onAutoRefresh(autoRefreshButtonOFF, autoRefreshButtonTEN, buttonValues,
 
 
 }
+
+function imageClick(){
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+}
+
 
 
