2f62bf9d8a918bd2d325b9b5aeff0130fb470548
arapocket <arakazaryan@me.com>
2017-10-23 18:39:13 -0700

    changed panel colors

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -393,7 +393,6 @@ function loadRoutesOnMap(checkpoints, map, iconsBase, route, routeMarkers) {
     route.setPath([]);
     for (i = 0 ; i < routeMarkers.length ; i++){
         routeMarkers[i].setMap(null);
-        console.log(routeMarkers[i]);
     }
     for (i = 0; i < checkpoints.length; i++) {
         var latLng = new google.maps.LatLng(checkpoints[i].lat, checkpoints[i].lng);
