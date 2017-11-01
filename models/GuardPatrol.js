31ef8b689aff9404d9ec523a58dbd080f59d845b
arapocket <arakazaryan@me.com>
2017-10-31 23:18:24 -0700

    syntax

--- models/GuardPatrol.js
+++ models/GuardPatrol.js
@@ -1,7 +1,7 @@
 var db = require('./db');
 
 var app = require('express')();
-var http = require('http').Server(express);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
