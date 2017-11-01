53a63d30b1bce46112b2c603c2a666f0ffa65d85
arapocket <arakazaryan@me.com>
2017-10-31 21:31:21 -0700

    another test

--- routes/index.js
+++ routes/index.js
@@ -233,20 +233,5 @@ router.get('/currentroutes', Routes.getCurrentRoutes);
 
 
 // ###### Tue Oct 31 20:57:55 PDT 2017 ARA
-var http = require('http').Server(express);
-var io = require('socket.io')(http);
-
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-	  console.log('user disconnected');
-	});
-	socket.on('message', function(msg){
-	  console.log('message: ' + msg);
-	});
-	socket.on('message', function(msg){
-	  io.emit('message', msg);
-	});
-  });
 
 module.exports = router;
