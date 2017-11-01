ade0d101d3bfb6ed9b1eaeaabb75813a4ebe5d18
arapocket <arakazaryan@me.com>
2017-10-31 23:15:18 -0700

    notha notha notha test

--- models/GuardPatrol.js
+++ models/GuardPatrol.js
@@ -1,6 +1,22 @@
 var db = require('./db');
 
 
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+});
+
+
 
 
 // GETS ALL THE GUARDS WITH A CURRENT SHIFT
