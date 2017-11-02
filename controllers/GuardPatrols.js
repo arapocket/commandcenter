d0c3550b1ab3de63429b55d21f9e59c321e0cdcf
arapocket <arakazaryan@me.com>
2017-11-02 09:13:56 -0700

    changed port to 3001

--- controllers/GuardPatrols.js
+++ controllers/GuardPatrols.js
@@ -4,6 +4,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
