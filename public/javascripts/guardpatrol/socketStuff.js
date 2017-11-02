cb17c9925405af0b9ae8f14d9b28c4cb1eb7fe0c
arapocket <arakazaryan@me.com>
2017-11-02 14:13:54 -0700

    test

--- public/javascripts/guardpatrol/socketStuff.js
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -1,12 +1,12 @@
 $(function () {
     var socket = io();
-    $('form').submit(function(){
-    socket.emit('message', $('#m').val());
-    $('#m').val('');
-    return false;
-    });
-    socket.on('message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-    window.scrollTo(0, document.body.scrollHeight);
-    });
-    });
+    // $('form').submit(function(){
+    // socket.emit('message', $('#m').val());
+    // $('#m').val('');
+    // return false;
+    // });
+    // socket.on('message', function(msg){
+    // $('#messages').append($('<li>').text(msg));
+    // window.scrollTo(0, document.body.scrollHeight);
+    // });
+    // });
