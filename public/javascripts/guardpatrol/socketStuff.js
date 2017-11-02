94dd6eec3ccb2cab6d20a5e8412e6b3f87825a92
arapocket <arakazaryan@me.com>
2017-11-02 14:07:31 -0700

    created separate js for socket stuff

---
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -0,0 +1,12 @@
+$(function () {
+    var socket = io();
+    $('form').submit(function(){
+    socket.emit('message', $('#m').val());
+    $('#m').val('');
+    return false;
+    });
+    socket.on('message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+    window.scrollTo(0, document.body.scrollHeight);
+    });
+    });
