227fa753fea718ce80bb931b08a610e57ab288e7
arapocket <arakazaryan@me.com>
2017-11-03 13:06:45 -0700

    working

--- public/javascripts/guardpatrol/socketStuff.js
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -1,23 +1,23 @@
 function socketStuff() {
 
 
-    // console.log("logging io");
-    // console.log(io);
+    console.log("logging io");
+    console.log(io);
 
     $(function () {
 
-        var socket = io("http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3001");        
-        // console.log("logging socket");
-        // console.log(socket);
-    //     $('form').submit(function () {
-    //         socket.emit('message', $('#m').val());
-    //         $('#m').val('');
-    //         return false;
-    //     });
-    //     socket.on('message', function (msg) {
-    //         $('#messages').append($('<li>').text(msg));
-    //         window.scrollTo(0, document.body.scrollHeight);
-    //     });
+        var socket = io();        
+        console.log("logging socket");
+        console.log(socket);
+        $('form').submit(function () {
+            socket.emit('message', $('#m').val());
+            $('#m').val('');
+            return false;
+        });
+        socket.on('message', function (msg) {
+            $('#messages').append($('<li>').text(msg));
+            window.scrollTo(0, document.body.scrollHeight);
+        });
     });
 
 }
