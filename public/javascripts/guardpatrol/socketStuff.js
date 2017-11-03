524acd5605c1d10f47195c016be0fd21ba22e04b
arapocket <arakazaryan@me.com>
2017-11-03 11:13:18 -0700

    conectionOptions

--- public/javascripts/guardpatrol/socketStuff.js
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -6,7 +6,16 @@ function socketStuff() {
 
     $(function () {
 
-        var socket = io("http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/guardpatrols");        
+
+        var connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+            "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
+            "transports" : ["websocket"]
+        };
+        
+
+        var socket = io("http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/guardpatrols", connectionOptions);        
         console.log("logging socket");
         console.log(socket);
         $('form').submit(function () {
