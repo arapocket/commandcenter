73a13207b0d4213895073bdbdf7637be2d9ffe2f
arapocket <arakazaryan@me.com>
2017-11-10 12:57:20 -0800

    initMap is in res function

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -1,8 +1,25 @@
-function initMap() {
-
+function getMapContents(){
+    var xhr = new XMLHttpRequest();
     
-    getMapContents();
+        if (!xhr) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+    
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {   
+                initMap(); 
+            }
+        }
+    
+        xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/guardpatrols/", true);
+    
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(null);
+}
 
+
+function initMap() {
     locations = parent.locations;
     incidents = parent.incidents;
     guards = parent.guards;
@@ -66,25 +83,6 @@ function initMap() {
         currentButtonValue = onAutoRefresh(autoRefreshButtonOFF, autoRefreshButtonTEN, buttonValues, currentButtonValue);
     });
 
-}
-
-function getMapContents(){
-    var xhr = new XMLHttpRequest();
-    
-        if (!xhr) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
-    
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE) {    
-            }
-        }
-    
-        xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/guardpatrols/", true);
-    
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(null);
 }
 
 function timedRefresh(timeoutPeriod) {
