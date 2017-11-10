6912bca74aed6a4a8eeceb6679a26b38840ed4f4
arapocket <arakazaryan@me.com>
2017-11-10 13:33:08 -0800

    with cherry

--- models/foxmap.js
+++ models/foxmap.js
@@ -1,12 +1,24 @@
 var db = require('./db');
 
 module.exports.getMap = function (callback) {
-    db.createConnection(function (err, res) {
+    db.createConnection(function (err, reslt) {
 
         if (err){
             callback(err, null);
         }else{
-            callback(res);
+            var connection = reslt;
+            var strSQL = ' SELECT * FROM Guard';
+            connection.query(strSQL, function (err, rows, fields) {
+                if (!err) {
+                    connection.end();
+                    callback(null, rows);
+
+                } else {
+                    console.log('error with the select guardpatrol query');
+                    connection.end();
+                    callback(err, rows);
+                }
+            });
         }
         
     });
