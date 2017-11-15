1ecf8ec081f4d9f367bb0bbe3c08b70d82c21bcf
arapocket <arakazaryan@me.com>
2017-11-15 15:24:35 -0800

    test C

--- public/javascripts/guardpatrol/renderIncidentDetails.js
+++ public/javascripts/guardpatrol/renderIncidentDetails.js
@@ -3,6 +3,6 @@ console.log("renderIncidentDetails called");
 
 var incidentPhoto = document.getElementById("incidentPhoto");
 
-incidentPhoto.data = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident.IncidentID;
+incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident.IncidentID;
 
 }
