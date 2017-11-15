85587436af77446da5181fc6f198d89ad47fa80a
arapocket <arakazaryan@me.com>
2017-11-15 10:37:39 -0800

    limited colors for usernames

--- public/javascripts/guardpatrol/socketStuff.js
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -6,9 +6,9 @@
         var FADE_TIME = 150; // ms
         var TYPING_TIMER_LENGTH = 400; // ms
         var COLORS = [
-          '#e21400', '#91580f', '#f8a700', '#f78b00',
-          '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-          '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+          '#e21400', '#f8a700', '#f78b00',
+          '#58dc00', '#a8f07a', '#4ae8c4',
+          '#3b88eb'
         ];
       
         /// Initialize variables
