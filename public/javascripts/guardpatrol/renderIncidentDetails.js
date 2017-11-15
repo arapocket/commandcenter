010121dd20928f4870983ec169ce06b52a5d6e7f
arapocket <arakazaryan@me.com>
2017-11-15 15:42:46 -0800

    object always comes back as array

--- public/javascripts/guardpatrol/renderIncidentDetails.js
+++ public/javascripts/guardpatrol/renderIncidentDetails.js
@@ -3,6 +3,6 @@ console.log("renderIncidentDetails called");
 console.log(incident);
 var incidentPhoto = document.getElementById("incidentPhoto");
 
-incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident.IncidentID;
+incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
 
 }
