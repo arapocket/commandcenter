5d3313243ee2d94181e0f6877d69d8b7fcf56faf
arapocket <arakazaryan@me.com>
2017-11-15 13:22:46 -0800

    console log

--- public/javascripts/guardpatrol/incidentdetail.js
+++ public/javascripts/guardpatrol/incidentdetail.js
@@ -1,5 +1,7 @@
 function incidentDetail(){
 
+    console.log("incidentDetail called ");
+
     var incidentID = parent.incidents[i].IncidentID;
     var pic = document.getElementById("incidentphoto");
     pic.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incidentID;
