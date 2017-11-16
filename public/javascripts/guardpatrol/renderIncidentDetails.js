0d6825445d637b4f3ab6de33279543b3af0f7311
arapocket <arakazaryan@me.com>
2017-11-15 16:12:52 -0800

    test E

--- public/javascripts/guardpatrol/renderIncidentDetails.js
+++ public/javascripts/guardpatrol/renderIncidentDetails.js
@@ -2,7 +2,19 @@ function renderIncidentDetails(){
 console.log("renderIncidentDetails called");
 console.log(incident);
 var incidentPhoto = document.getElementById("incidentPhoto");
-
+var backButton = document.getElementById("backButton");
 incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
 
+backButton.addEventListener('click', function (e) {
+            backToMap();
+        });
+
+
+}
+
+
+function backToMap(){
+
+    window.location.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/foxmaps";
+    
 }
