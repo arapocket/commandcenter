cc5e9bc3ac43954fe8874af0f0b6c994565044ae
arapocket <arakazaryan@me.com>
2017-12-28 11:48:28 -0800

    'secret'

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ console.log(token);
 
 var expressJwt = require('express-jwt');
 app.use(expressJwt({
-  secret: secret,
+  secret: 'secret',
   getToken: function fromHeaderOrQuerystring (req) {
       if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
           return req.headers.authorization.split(' ')[1   ];
