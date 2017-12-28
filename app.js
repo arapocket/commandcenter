a2a69a4f47f872cfe16b6653250c658d00f9c161
arapocket <arakazaryan@me.com>
2017-12-28 09:35:31 -0800

    trying out the usage sample code

--- app.js
+++ app.js
@@ -296,7 +296,14 @@ io.on('connection', function (socket) {
 });
 
 
+var jwt = require('express-jwt');
 
+app.get('/protected',
+  jwt({secret: 'shhhhhhared-secret'}),
+  function(req, res) {
+    if (!req.user.admin) return res.sendStatus(401);
+    res.sendStatus(200);
+  });
 
 
 
