19325390f163ae7ac4c10ec3eb8fe63355177b91
arapocket <arakazaryan@me.com>
2018-01-05 23:17:16 -0800

    moved notification stuff inside io.on(connection)

--- app.js
+++ app.js
@@ -222,59 +222,13 @@ if (process.env.CC_SSL == "YES"){
 //}
 
 
-//////////////////////////////// ###### Wed Oct 4 18:39:53 PDT 2017 ARA
-
-const httpOptions = {
-  hostname: 'http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com',
-  port: 3000,
-  path: '/guards',
-  method: 'GET',
-  headers: {
-  }
-}
-
-const tokenReq = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-
-tokenReq.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-
-const apn = require("apn");
-
-const notificationOptions = {
-  token: {
-    key: "C:/Users/Administrator/greyfox/certificates/AuthKey_M4D8R5539E.p8",
-    keyId: "M4D8R5539E",
-    teamId: "KUJ3K83XLJ"
-  },
-  production: false
-};
-
-var apnProvider = new apn.Provider(notificationOptions);
-let deviceToken = "94b9c26276c600d067a09803bfae74611b7e1b91872e72567df15d5040ad681b" 
-
-var note = new apn.Notification();
-
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-note.sound = "ping.aiff";
-note.badge = 0;
-note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-note.payload = {'messageFrom': 'GREYFOX'};
-note.topic = "mobss.foxwatchrn";
-
-////////////////////////////////////////////////////////////////////////////
-
+/*
+=====================================================================
+=====================================================================
+              ###### Wed Oct 4 18:39:53 PDT 2017 ARA
+=====================================================================
+=====================================================================
+**/
 
 
 var app = express();
@@ -286,6 +240,60 @@ var io = require('socket.io')(server);
 var numUsers = 0;
 
 io.on('connection', function (socket) {
+
+///////////////////////////////NOTIFICATION STUFF//////////////////////////
+  const httpOptions = {
+    hostname: 'http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com',
+    port: 3000,
+    path: '/guards',
+    method: 'GET',
+    headers: {
+    }
+  }
+  
+  const tokenReq = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+  
+  tokenReq.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  
+  
+  const apn = require("apn");
+  
+  const notificationOptions = {
+    token: {
+      key: "C:/Users/Administrator/greyfox/certificates/AuthKey_M4D8R5539E.p8",
+      keyId: "M4D8R5539E",
+      teamId: "KUJ3K83XLJ"
+    },
+    production: false
+  };
+  
+  var apnProvider = new apn.Provider(notificationOptions);
+  let deviceToken = "94b9c26276c600d067a09803bfae74611b7e1b91872e72567df15d5040ad681b" 
+  
+  var note = new apn.Notification();
+  
+  note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+  note.sound = "ping.aiff";
+  note.badge = 0;
+  note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+  note.payload = {'messageFrom': 'GREYFOX'};
+  note.topic = "mobss.foxwatchrn";
+  
+  ///////////////////////////////SOCKET STUFF//////////////////////////
+
+
   var addedUser = false;
 
   // when the client emits 'message', this listens and executes
