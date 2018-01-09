64c0e9648cade19ba0e9d25ef9134c4f2a81d9b3
arapocket <arakazaryan@me.com>
2018-01-09 15:15:39 -0800

    idj

--- public/javascripts/createInviteList.js
+++ public/javascripts/createInviteList.js
@@ -1,9 +1,17 @@
-function toggle(source) {
+function loadScripts() {
 
-    console.log('toggle this called');
 
-    checkboxes = document.getElementsByName('person');
-    for (var i = 0, n = checkboxes.length; i < n; i++) {
-        checkboxes[i].checked = '';
-    }
+    var checkboxes = document.getElementsByName('person');
+    var checkallbox = document.getElementById('checkallbox');
+
+    checkallbox.addEventListener('click', function(e){
+        console.log('checkallbox clicked');
+
+        for (var i = 0, n = checkboxes.length; i < n; i++) {
+            checkboxes[i].checked = '';
+        }
+
+    })
+
+
 }
