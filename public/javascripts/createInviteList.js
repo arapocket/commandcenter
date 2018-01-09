ad8f91e12b898317c32059b7402c83740ac8a152
arapocket <arakazaryan@me.com>
2018-01-09 15:03:03 -0800

    isdja

--- public/javascripts/createInviteList.js
+++ public/javascripts/createInviteList.js
@@ -1,4 +1,7 @@
 function toggle(source) {
+
+    console.log('toggle this called');
+
     checkboxes = document.getElementsByName('person');
     for (var i = 0, n = checkboxes.length; i < n; i++) {
         checkboxes[i].checked = '';
