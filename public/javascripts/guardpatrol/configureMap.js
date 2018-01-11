a5b45bb8e5d0eddd4cfa9232666ce4939f25f26e
arapocket <arakazaryan@me.com>
2018-01-10 21:02:45 -0800

    added anchors in script

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -187,10 +187,8 @@ function createIncidentMarkers(incidents, map, iconsBase) {
         <h5 style="color:#D20202">`  + incidents[i].Type + `</h5>
         <h6 style="color:#404040"> 
         ` + incidents[i].Description + `
-        </h6> ` + `  <a href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/`
-        + incidents[i].IncidentID +
-        `">         ` +    
-         `<object id = 'map' data='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentpreview/` + incidents[i].IncidentID +  `' width='100%' height='100%' type='text/html'> <object/> </a> `
+        </h6> ` +    
+         `<object id = 'map' data='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentpreview/` + incidents[i].IncidentID +  `' width='100%' height='100%' type='text/html'> <object/> `
 
 
 
--- public/javascripts/guardpatrol/renderIncidentPreview.js
+++ public/javascripts/guardpatrol/renderIncidentPreview.js
@@ -1,5 +1,12 @@
 function renderIncidentPreview(){
 
+    var imageAnchor = document.getElementById('imageAnchor');
+    var videoAnchor = document.getElementById('videoAnchor');
+
+    imageAnchor.href  = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
+    videoAnchor.href  = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
+
+
     var incidentPhoto = document.getElementById("incidentPhoto");
     var incidentVideo = document.getElementById('incidentVideo');
     incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
