8271f4a4620bfd2dfe9163ca41a649552bfe4d2a
arapocket <arakazaryan@me.com>
2018-01-11 10:55:04 -0800

    then set the new route to 1

--- models/Route.js
+++ models/Route.js
@@ -150,7 +150,7 @@ module.exports.updateRoute = function (Route, callback) {
                     // connection.end();
                     callback(null, rows);
                     // here we will set our selected route to 1
-                    var strSQL2 = "Update route SET CurrentRoute = " + Route.CurrentRoute + " WHERE GuardID = '" + Route.GuardID + "' AND WHERE  RouteID = '" + Route.RouteID + "';";
+                    var strSQL2 = "Update route SET CurrentRoute = " + Route.CurrentRoute + " WHERE RouteID = '" + Route.RouteID + "';";
                     connection.query(strSQL2, function (err, rows, fields) {
                         if (!err) {
                             connection.end();
