e60d1e53ecb85a4f1f7d9842005d2ca2a18132bd
arapocket <arakazaryan@me.com>
2018-01-15 15:50:47 -0800

    added more code

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -195,33 +195,37 @@ function initMap() {
 
     function changeButtons(GuardID, locations, map, iconsBase){
 
+        
 
         for (i = 0 ; i < locations.length ; i++ ){
-            let hideRemoveButton = parent.document.getElementById(locations[i].GuardID + 'remove');
-            let hideSaveButton = parent.document.getElementById(locations[i].GuardID + 'save');
-            let hideLoadButton = parent.document.getElementById(locations[i].GuardID + 'load');
+            var id = locations[i].GuardID;
+
+            let hideTrashButton = parent.document.getElementById('trashRouteButton' + id);
+            let hideAddButton = parent.document.getElementById('addRouteButton' + id);
+            let hideRemoveButton = parent.document.getElementById('removeCheckpointButton' + id);
+            let hideSaveButton = parent.document.getElementById('saveRouteButton' + id);
+            let hideLoadButton = parent.document.getElementById('loadRouteButton' + id);
             
+            hideTrashButton.style.display = 'none';
+            hideAddButton.style.display = 'none';
             hideRemoveButton.style.display = 'none';
             hideSaveButton.style.display = 'none';
             hideLoadButton.style.display = 'none';        
         }
     
-        // google.maps.event.clearListeners(map, 'click');
-    
-        // var route = JSON.parse(localStorage.getItem(GuardID + 'route'));
-        // var routeMarkers = JSON.parse(localStorage.getItem(GuardID + 'routeMarkers'));
-    
-        // map.addListener('click', function (e) {
-        //     onSetCheckpoint(route, e.latLng, map, iconsBase, routeMarkers);
-        // });
-    
-        let removeButton = parent.document.getElementById(GuardID + 'remove');
-        let saveButton = parent.document.getElementById(GuardID + 'save');
-        let loadButton = parent.document.getElementById(GuardID + 'load');
-    
-        removeButton.style.display = 'block';
-        saveButton.style.display = 'block';
-        loadButton.style.display = "block";
+
+        let trashRouteButton = parent.document.getElementById('trashRouteButton' + GuardID);
+        let addRouteButton = parent.document.getElementById('addRouteButton' + GuardID);
+        let removeCheckpointButton = parent.document.getElementById('removeCheckpointButton' + GuardID);
+        let saveRouteButton = parent.document.getElementById('saveRouteButton' + GuardID);
+        let loadRouteButton = parent.document.getElementById('loadRouteButton' + GuardID);
+        
+        trashRouteButton.style.display = 'block';
+        addRouteButton.style.display = 'block';
+        removeCheckpointButton.style.display = 'block';
+        saveRouteButton.style.display = 'block';
+        loadRouteButton.style.display = 'block';        
+    }
     }
 
     function createIncidentButtons(map, incidents) {
