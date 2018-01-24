ca020060d1a08df6a7a6fac9a904abf4077065dd
arapocket <arakazaryan@me.com>
2018-01-24 11:50:35 -0800

    ping no longer works so i changed the text tone

--- public/javascripts/guardpatrol/locationIsAccurate.js
+++ public/javascripts/guardpatrol/locationIsAccurate.js
@@ -13,11 +13,11 @@ function locationIsAccurate(location, lastLocation) {
     
         //CHECK IF DISTANCE IS TOO FAR, ie PREVENT WEIRD LINES ON MAP
     
-        // console.log("Comparing this location and last location:");
-        // console.log("Current Latitude: " + location.coords.latitude);
-        // console.log("Current Longitude: " + location.coords.longitude)
-        // console.log("Last Latitude: " + this.lastLocation.coords.latitude);
-        // console.log("Last Longitude: " + this.lastLocation.coords.longitude);
+        console.log("Comparing this location and last location:");
+        console.log("Current Latitude: " + location.coords.latitude);
+        console.log("Current Longitude: " + location.coords.longitude)
+        console.log("Last Latitude: " + this.lastLocation.coords.latitude);
+        console.log("Last Longitude: " + this.lastLocation.coords.longitude);
         console.log("Delta Latitude: " + deltaLatitude);
         console.log("Delta Longitude: " + deltaLongitude);
     
