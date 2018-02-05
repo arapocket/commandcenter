87a5120cc49b097a53fa5da24b13933e4d76c9be
arapocket <arakazaryan@me.com>
2018-02-05 14:30:13 -0800

    aisdj

--- public/javascripts/guardpatrol/renderIncidentPreview.js
+++ public/javascripts/guardpatrol/renderIncidentPreview.js
@@ -3,33 +3,36 @@ function renderIncidentPreview() {
     console.log('rendering incident media type from renderIncidentPreview');
     console.log(incident[0].Media);
 
+    let imageAnchor = document.getElementById('imageAnchor');
+    let videoAnchor = document.getElementById('videoAnchor');
+    let incidentPhoto = document.getElementById("incidentPhoto");
+    let incidentVideo = document.getElementById('incidentVideo');
+
+
     if (incident[0].Media == 'none') {
         console.log('none called');
+        incidentPhoto.style.display = 'none';
+        incidentVideo.style.display = 'none';
     }
-
 
     if (incident[0].Media == 'photo') {
 
         console.log('photo called');
 
-        var imageAnchor = document.getElementById('imageAnchor');
         imageAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
-        var incidentPhoto = document.getElementById("incidentPhoto");
         incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentPhoto.style.display = 'block';
-
+        incidentVideo.style.display = 'none';
     }
-
 
     if (incident[0].Media == 'video') {
 
         console.log('video called');
 
-        var videoAnchor = document.getElementById('videoAnchor');
         videoAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
-        var incidentVideo = document.getElementById('incidentVideo');
         incidentVideo.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentVideo.style.display = 'block';
+        incidentPhoto.style.display = 'none';
 
     }
 }
