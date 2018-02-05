560727b24f34f15f8cbba2a38529e80aebeaf414
arapocket <arakazaryan@me.com>
2018-02-05 12:12:23 -0800

    console lgo

--- public/javascripts/guardpatrol/renderIncidentPreview.js
+++ public/javascripts/guardpatrol/renderIncidentPreview.js
@@ -1,12 +1,17 @@
 function renderIncidentPreview() {
 
+    console.log('rendering incident media type from renderIncidentPreview');
+    console.log(incident[0].Media);
 
     if (incident[0].Media == 'none') {
-
+        console.log('none called');
     }
 
 
     if (incident[0].Media == 'photo') {
+
+        console.log('photo called');
+
         var imageAnchor = document.getElementById('imageAnchor');
         imageAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
         var incidentPhoto = document.getElementById("incidentPhoto");
@@ -17,6 +22,9 @@ function renderIncidentPreview() {
 
 
     if (incident[0].Media == 'video') {
+
+        console.log('video called');
+
         var videoAnchor = document.getElementById('videoAnchor');
         videoAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
         var incidentVideo = document.getElementById('incidentVideo');
