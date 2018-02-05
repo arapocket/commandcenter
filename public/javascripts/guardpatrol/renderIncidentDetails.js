deb622af7f48df51ee89b0b00dac5268707b7a5d
arapocket <arakazaryan@me.com>
2018-02-05 14:28:17 -0800

    isdj

--- public/javascripts/guardpatrol/renderIncidentDetails.js
+++ public/javascripts/guardpatrol/renderIncidentDetails.js
@@ -1,5 +1,8 @@
 function renderIncidentDetails(){
 
+    let incidentPhoto = document.getElementById("incidentPhoto");
+    let incidentVideo = document.getElementById('incidentVideo');
+
     if (incident[0].Media == 'none') {
         incidentPhoto.style.display = 'none';
         incidentVideo.style.display = 'none';
@@ -7,19 +10,17 @@ function renderIncidentDetails(){
 
 
     if (incident[0].Media == 'photo') {
-        var incidentPhoto = document.getElementById("incidentPhoto");
+        
         incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentPhoto.style.display = 'block';
         incidentVideo.style.display = 'none';
     }
 
-
     if (incident[0].Media == 'video') {
-        var incidentVideo = document.getElementById('incidentVideo');
+        
         incidentVideo.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentVideo.style.display = 'block';
         incidentPhoto.style.display = 'none';
-
     }
 
 
