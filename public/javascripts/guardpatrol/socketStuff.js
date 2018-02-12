4d3d2d9459fb8d2e6ff7eda1699f978349be7699
arapocket <arakazaryan@me.com>
2018-02-12 14:52:29 -0800

    think i fixed the double message bug

--- public/javascripts/guardpatrol/socketStuff.js
+++ public/javascripts/guardpatrol/socketStuff.js
@@ -224,7 +224,7 @@ $(function () {
   });
 
   // Whenever the server emits 'message', update the chat body
-  socket.on('message', function (data) {
+  socket.on('new message', function (data) {
     addChatMessage(data);
   });
 
