d38048493844b2da94f31718f56564a0b03fc288
arapocket <arakazaryan@me.com>
2018-02-15 10:10:32 -0800

    woops

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -82,7 +82,7 @@ function initMap() {
                 map: map,
                 zIndex: 1,
                 geodesic: true,
-                strokeColor: "#red",
+                strokeColor: "red",
                 strokeOpacity: 1,
                 strokeWeight: 5,
                 icons: [routeSeq]
