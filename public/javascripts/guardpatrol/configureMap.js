98c056a669d37f982a17e9931548b7d7a5c652c1
arapocket <arakazaryan@me.com>
2018-02-16 17:40:20 -0800

    zoom out map mroe`:

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -57,7 +57,7 @@ function initMap() {
 
 
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 20,
+            zoom: 25,
             center: { lat: firstLocationLat, lng: firstLocationLng },
             mapTypeId: google.maps.MapTypeId.MAP,
             streetViewControl: false,
