469cea5c23f4b2553d22abf8d0bed9d8929be75f
arapocket <arakazaryan@me.com>
2018-02-19 12:45:29 -0800

    asidj

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -923,7 +923,6 @@ function initMap() {
 
         xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/checkpoints/" + routeID, true);
 
-        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(null);
     }
 
