f79742fdf66c1b210d2063d36129828ecba31e3d
arapocket <arakazaryan@me.com>
2018-02-19 09:48:19 -0800

    sdij

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -667,9 +667,9 @@ function initMap() {
         xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/selectedroute/", true);
 
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send({
-            "RouteName": routeName
-        });
+        xhr.send(JSON.stringify({
+            "RouteName": 'aras house'
+        }));
     }
 
 
