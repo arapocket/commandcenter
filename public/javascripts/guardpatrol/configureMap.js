c6a7afdcc06f061f39dbd5c2436f39a96d3ba91f
arapocket <arakazaryan@me.com>
2018-02-19 11:26:36 -0800

    k don't set the headers again

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -713,7 +713,6 @@ function initMap() {
         
         xhr.open("PUT", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/routes", true);
 
-        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
             "CurrentRoute": 1,
             "NotCurrentRoute": 0,
