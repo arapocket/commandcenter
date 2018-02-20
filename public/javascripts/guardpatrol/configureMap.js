c708d2f2520409d055259496d4643cb70a9de656
arapocket <arakazaryan@me.com>
2018-02-19 18:25:38 -0800

    not sure if i need loadCurrentRoutes

--- public/javascripts/guardpatrol/configureMap.js
+++ public/javascripts/guardpatrol/configureMap.js
@@ -106,6 +106,7 @@ function initMap() {
             strokeOpacity: 1
         }
     };
+
     let route = new google.maps.Polyline({
         map: map,
         zIndex: 1,
@@ -673,7 +674,7 @@ function initMap() {
 
     function hideAddButton() {
         let addRouteButton = parent.document.getElementById('addRouteButton');
-        addRouteButton.style.display = 'none'        
+        addRouteButton.style.display = 'none'
     }
 
     function showAddButton() {
@@ -955,7 +956,7 @@ function initMap() {
                 if (json.length > 0) {
                     let routeID = json[0].RouteID;
                     setCurrentRoute(json[0], map, route);
-                    loadCurrentRoutes(json[0].RouteID, map, route);
+                    // loadCurrentRoutes(json[0].RouteID, map, route);
                 }
             }
         }
@@ -984,7 +985,7 @@ function initMap() {
                 let json = JSON.parse(xhr.responseText);
                 if (json.length > 0) {
                     let routeID = json[0].RouteID;
-                    loadCurrentRoutes(routeID, map, route);
+                    // loadCurrentRoutes(routeID, map, route);
                 }
 
 
@@ -1097,30 +1098,30 @@ function initMap() {
         bootbox.alert('Route has been saved for later!')
     }
 
-    function loadCurrentRoutes(routeID, map, route) {
+    // function loadCurrentRoutes(routeID, map, route) {
 
-        console.log('loadCurrentRoutes called ');
+    //     console.log('loadCurrentRoutes called ');
 
-        var xhr = new XMLHttpRequest();
+    //     var xhr = new XMLHttpRequest();
 
-        if (!xhr) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
+    //     if (!xhr) {
+    //         alert('Giving up :( Cannot create an XMLHTTP instance');
+    //         return false;
+    //     }
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                console.log('logging checkpoints response: ' + xhr.responseText);
-                var checkpoints = JSON.parse(xhr.responseText);
-                loadRoutesOnMap(checkpoints, map, route);
+    //     xhr.onreadystatechange = function () {
+    //         if (xhr.readyState == XMLHttpRequest.DONE) {
+    //             console.log('logging checkpoints response: ' + xhr.responseText);
+    //             var checkpoints = JSON.parse(xhr.responseText);
+    //             loadRoutesOnMap(checkpoints, map, route);
 
-            }
-        }
+    //         }
+    //     }
 
-        xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/checkpoints/" + routeID, true);
+    //     xhr.open("GET", "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/checkpoints/" + routeID, true);
 
-        xhr.send(null);
-    }
+    //     xhr.send(null);
+    // }
 
     function loadRoutesOnMap(checkpoints, map, route) {
 
