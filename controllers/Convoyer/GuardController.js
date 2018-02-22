3c451afc5608a2de24efbf02d0abebbdee26d99a
arapocket <arakazaryan@me.com>
2018-02-21 16:37:22 -0800

    sidj

--- controllers/Convoyer/GuardController.js
+++ controllers/Convoyer/GuardController.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var GuardModel = require('../../models/Convoyer/GuardModel');
 var db = require('../../models/db');
-var datetime = require('./datetime');
+var datetime = require('../datetime');
 
 
 module.exports.guardList = function (req, res) {
