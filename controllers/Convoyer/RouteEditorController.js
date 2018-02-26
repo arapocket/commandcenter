51f884e426a172b185fe484b1724db62a19474aa
arapocket <arakazaryan@me.com>
2018-02-26 10:13:58 -0800

    adsij

--- controllers/Convoyer/RouteEditorController.js
+++ controllers/Convoyer/RouteEditorController.js
@@ -5,15 +5,15 @@ module.exports.getRouteEditor = (function (req, res) {
 
   RouteEditorModel.getAllRoutes(function (err, getAllRoutesResult) {
     if (err) {
-      res.json(err);
+      // res.json(err);
     } else {
       RouteEditorModel.getAllGuards(function (err, getAllGuardsResult) {
         if (err) {
-          res.json(err);
+          // res.json(err);
         } else {
           RouteEditorModel.getAllPatrolAreas(function (err, getAllPatrolAreasResult){
             if (err) {
-              res.json(err);
+              // res.json(err);
             } else {
               res.render('RouteEditorView', { title: 'Route Editor', getAllGuardsResult: getAllGuardsResult, getAllRoutesResult: getAllRoutesResult, getAllPatrolAreasResult: getAllPatrolAreasResult });
             }
