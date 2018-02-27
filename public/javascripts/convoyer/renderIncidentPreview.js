75bee81dab2513c1d124a49f3382c8ea5599d656
arapocket <arakazaryan@me.com>
2018-02-27 15:58:22 -0800

    aija

--- public/javascripts/convoyer/renderIncidentPreview.js
+++ public/javascripts/convoyer/renderIncidentPreview.js
@@ -19,7 +19,7 @@ function renderIncidentPreview() {
 
         console.log('photo called');
 
-        imageAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetailsmap/" + incident[0].IncidentID
+        imageAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
         incidentPhoto.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentPhoto.style.display = 'block';
         incidentVideo.style.display = 'none';
@@ -29,7 +29,7 @@ function renderIncidentPreview() {
 
         console.log('video called');
 
-        videoAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetailsmap/" + incident[0].IncidentID
+        videoAnchor.href = "http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/" + incident[0].IncidentID
         incidentVideo.src = "https://s3-us-west-2.amazonaws.com/foxwatch/" + incident[0].IncidentID;
         incidentVideo.style.display = 'block';
         incidentPhoto.style.display = 'none';
