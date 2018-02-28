cb831e3a7e4cafac339f671c987e92ee84062fda
arapocket <arakazaryan@me.com>
2018-02-28 13:09:17 -0800

    asijd

--- public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
+++ public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
@@ -1,7 +1,8 @@
 
 function initMap() {
 
-    let center = { lat: results[0].lat, lng: results[0].lng };
+    let guard = results[0];
+    let center = { lat: guard.lat, lng: guard.lng };
 
     const COLORS = [
         '#e21400', '#f8a700', '#f78b00',
@@ -22,6 +23,80 @@ function initMap() {
             panControl: false,
             rotateControl: false,
         });
+
+        createGuardMarker();
     
+        createPatrolPath();
+
+
+        function createGuardMarker() {
+
+            let marker = new google.maps.Marker({
+                position: { lat: guard.lat, lng: guard.lng },
+                map: map,
+                animation: google.maps.Animation.DROP,
+            });
+        }
+    
+        function createPatrolPath() {
+
+            let firstName = guard.FirstName;
+    
+            let pathColor = getPathColor(firstName);
+    
+            var patrolSeq = {
+                repeat: '30px',
+                icon: {
+                    path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+                    scale: 1,
+                    fillOpacity: 0,
+                    strokeColor: "red",
+                    strokeWeight: 1,
+                    strokeOpacity: 1
+                }
+            };
+            var patrol = new google.maps.Polyline({
+                map: map,
+                zIndex: 1,
+                geodesic: true,
+                strokeColor: pathColor,
+                strokeOpacity: 1,
+                strokeWeight: 3,
+                icons: [patrolSeq]
+            })
+    
+    
+            for (let i = 0; i < results.length; i++) {
+
+                    let latLng = new google.maps.LatLng(results[i].lat, results[i].lng);
+                    if (i > 0) {
+                        let lastLocation = new google.maps.LatLng(results[i - 1].lat, results[i - 1].lng);
+                        let locAccurate = locationIsAccurate(latLng, lastLocation);
+                        if (locAccurate) {
+                            patrol.getPath().push(latLng);
+                        } else {
+                            patrol.getPath().pop();
+                        }
+                    } else {
+                        patrol.getPath().push(latLng);
+                    }
+                
+            }
+    
+        }
+
+
+        function getPathColor(username) {
+            // Compute hash code
+            var hash = 7;
+            for (var i = 0; i < username.length; i++) {
+                hash = username.charCodeAt(i) + (hash << 5) - hash;
+            }
+            // Calculate color
+            var index = Math.abs(hash % COLORS.length);
+            return COLORS[index];
+        }
+            
+
     }
     
