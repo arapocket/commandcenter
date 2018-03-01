285e0e90e3b36f8875b12157920a359efb7b719d
arapocket <arakazaryan@me.com>
2018-03-01 09:41:49 -0800

    asdij

--- public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
+++ public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
@@ -11,97 +11,96 @@ function initMap() {
     ];
 
     var iconsBase = "http://maps.google.com/mapfiles/"
-    
-        let map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 18,
-            center: center,
-            mapTypeId: google.maps.MapTypeId.MAP,
-            streetViewControl: false,
-            clickableIcons: false,
-            fullscreenControl: false,
-            mapTypeControl: true,
-            panControl: false,
-            rotateControl: false,
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.MAP,
+        streetViewControl: false,
+        clickableIcons: false,
+        fullscreenControl: false,
+        mapTypeControl: true,
+        panControl: false,
+        rotateControl: false,
+    });
+
+    createGuardMarker();
+
+    createPatrolPath();
+
+
+    function createGuardMarker() {
+
+        let marker = new google.maps.Marker({
+            position: { lat: guard.lat, lng: guard.lng },
+            map: map,
+            animation: google.maps.Animation.DROP,
         });
+    }
 
-        createGuardMarker();
-    
-        createPatrolPath();
+    function createPatrolPath() {
+
+        let firstName = guard.FirstName;
+
+        let pathColor = getPathColor(firstName);
+
+        var patrolSeq = {
+            repeat: '30px',
+            icon: {
+                path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+                scale: 1,
+                fillOpacity: 0,
+                strokeColor: "red",
+                strokeWeight: 1,
+                strokeOpacity: 1
+            }
+        };
+        var patrol = new google.maps.Polyline({
+            map: map,
+            zIndex: 1,
+            geodesic: true,
+            strokeColor: pathColor,
+            strokeOpacity: 1,
+            strokeWeight: 3,
+            icons: [patrolSeq]
+        })
 
 
-        function createGuardMarker() {
+        for (let i = 0; i < results.length; i++) {
 
-            let marker = new google.maps.Marker({
-                position: { lat: guard.lat, lng: guard.lng },
-                map: map,
-                animation: google.maps.Animation.DROP,
-            });
-        }
-    
-        function createPatrolPath() {
-
-            let firstName = guard.FirstName;
-    
-            let pathColor = getPathColor(firstName);
-    
-            var patrolSeq = {
-                repeat: '30px',
-                icon: {
-                    path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-                    scale: 1,
-                    fillOpacity: 0,
-                    strokeColor: "red",
-                    strokeWeight: 1,
-                    strokeOpacity: 1
+            let latLng = new google.maps.LatLng(results[i].lat, results[i].lng);
+            if (i > 0) {
+                let lastLocation = new google.maps.LatLng(results[i - 1].lat, results[i - 1].lng);
+                let locAccurate = locationIsAccurate(latLng, lastLocation);
+                if (locAccurate) {
+                    window.setTimeout(function () {
+                        patrol.getPath().push(latLng);
+                    }, 1000);
+                } else {
+                    patrol.getPath().pop();
                 }
-            };
-            var patrol = new google.maps.Polyline({
-                map: map,
-                zIndex: 1,
-                geodesic: true,
-                strokeColor: pathColor,
-                strokeOpacity: 1,
-                strokeWeight: 3,
-                icons: [patrolSeq]
-            })
-    
-    
-            for (let i = 0; i < results.length; i++) {
+            } else {
+                window.setTimeout(function () {
+                    patrol.getPath().push(latLng);
+                }, 1000);
 
-                    let latLng = new google.maps.LatLng(results[i].lat, results[i].lng);
-                    if (i > 0) {
-                        let lastLocation = new google.maps.LatLng(results[i - 1].lat, results[i - 1].lng);
-                        let locAccurate = locationIsAccurate(latLng, lastLocation);
-                        if (locAccurate) {
-                            setInterval(function() {
-                                patrol.getPath().push(latLng);
-                            }, 1000);
-                        } else {
-                            patrol.getPath().pop();
-                        }
-                    } else {
-                        setInterval(function() {
-                            patrol.getPath().push(latLng);
-                        }, 1000);
-                        
-                    }
-                
             }
-    
-        }
 
-
-        function getPathColor(username) {
-            // Compute hash code
-            var hash = 7;
-            for (var i = 0; i < username.length; i++) {
-                hash = username.charCodeAt(i) + (hash << 5) - hash;
-            }
-            // Calculate color
-            var index = Math.abs(hash % COLORS.length);
-            return COLORS[index];
         }
-            
 
     }
-    
+
+
+    function getPathColor(username) {
+        // Compute hash code
+        var hash = 7;
+        for (var i = 0; i < username.length; i++) {
+            hash = username.charCodeAt(i) + (hash << 5) - hash;
+        }
+        // Calculate color
+        var index = Math.abs(hash % COLORS.length);
+        return COLORS[index];
+    }
+
+
+}
