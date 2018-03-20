564475fbb456c92dc578c0176ec53eec30b3fffc
arapocket <arakazaryan@me.com>
2018-03-19 17:51:16 -0700

    wait 1 sec

--- public/javascripts/convoyer/convoyerMapScript.js
+++ public/javascripts/convoyer/convoyerMapScript.js
@@ -22,7 +22,7 @@ function initMap() {
     socket.on('incident', function (incident) {
         window.setTimeout(function () {
             parent.location.reload();
-        }, 10000);
+        }, 1000);
     });
 
     socket.on('first location', function (incident) {
