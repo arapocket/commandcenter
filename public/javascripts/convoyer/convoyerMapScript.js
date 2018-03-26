d0881c11519c404efefebaad02675c26afd56bb9
arapocket <arakazaryan@me.com>
2018-03-26 14:09:11 -0700

    sidjaij

--- public/javascripts/convoyer/convoyerMapScript.js
+++ public/javascripts/convoyer/convoyerMapScript.js
@@ -512,7 +512,7 @@ function initMap() {
 
             if (incidents[i].Media != 'none') {
                 windowString = `<h3>` + incidents[i].Type + ` </h3> 
-                    <p>` + incidents[i].Description + `</p> <object id = 'map' data='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentpreview/` + incidents[i].IncidentID + `' width='90%' height='100%' type='text/html'> <object/> 
+                    <p>` + incidents[i].Description + `</p> <object id = 'map' data='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentpreview/` + incidents[i].IncidentID + `' width='50%' height='50%' type='text/html'> <object/> 
                     `;
             } else {
                 windowString = `<h3>` + incidents[i].Type + ` </h3> <hr>
