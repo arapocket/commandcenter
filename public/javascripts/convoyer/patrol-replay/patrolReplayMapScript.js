e229c6ea824fa28676dabdcd6989fa7b37827763
arapocket <arakazaryan@me.com>
2018-03-26 09:55:43 -0700

    isdji1`

--- public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
+++ public/javascripts/convoyer/patrol-replay/patrolReplayMapScript.js
@@ -99,12 +99,12 @@ function initMap() {
 
                 if (incidents[i].Media == 'photo') {
                     windowString = `Incident Type: <br>`  + incidents[i].Type + ` <br>` + incidents[i].Description + `
-                    <a href ='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/` + incident[0].IncidentID + `' ><img src = 'https://s3-us-west-2.amazonaws.com/foxwatch/` + incident[0].IncidentID;`' >`;
+                    <a href ='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/` + incidents[i].IncidentID + `' ><img src = 'https://s3-us-west-2.amazonaws.com/foxwatch/` + incidents[i].IncidentID;`' >`;
                 } 
 
                 if (incidents[i].Media == 'video') {
                     windowString = `Incident Type: <br>`  + incidents[i].Type + ` <br>` + incidents[i].Description + `
-                    <a href ='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/` + incident[0].IncidentID + `' ><img src = 'https://s3-us-west-2.amazonaws.com/foxwatch/` + incident[0].IncidentID;`' >`;
+                    <a href ='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentdetails/` + incidents[i].IncidentID + `' ><img src = 'https://s3-us-west-2.amazonaws.com/foxwatch/` + incidents[i].IncidentID;`' >`;
                 }
                 // windowString = `Incident Type: <br>`  + incidents[i].Type + ` <br>` + incidents[i].Description;
 //<div><object id = 'map' data='http://ec2-34-210-155-178.us-west-2.compute.amazonaws.com:3000/incidentpreview/` + incidents[i].IncidentID + `' width='100%' height='100%' type='text/html'> <object/> </div> 
