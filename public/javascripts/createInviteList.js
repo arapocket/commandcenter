64ee152c6eb75771a14e60cfcebc37b8219e6203
arapocket <arakazaryan@me.com>
2018-05-29 13:58:05 -0700

    quick save

--- public/javascripts/createInviteList.js
+++ public/javascripts/createInviteList.js
@@ -3,53 +3,53 @@ function loadScripts() {
 
     var checkboxes = [];
 
-    for (i = 0 ; i < results.length ; i++){
-        
+    for (i = 0; i < results.length; i++) {
+
         let person = results[i];
         let checkbox = document.getElementById(person.EmpID);
 
-        if (checkbox != null || checkbox != undefined){
-            
-            checkbox.addEventListener('click', function(e) {
+        if (checkbox != null || checkbox != undefined) {
+
+            checkbox.addEventListener('click', function (e) {
                 console.log('a checkbox clicked');
             })
 
             checkboxes.push(checkbox);
-        }        
+        }
     }
 
     var checkallbox = document.getElementById('checkallbox');
 
     try {
-        checkallbox.addEventListener('click', function(e){
-            console.log('checkallbox clicked');  
-            
+        checkallbox.addEventListener('click', function (e) {
+            console.log('checkallbox clicked');
+
             console.log('logging checkboxes array');
             console.log(checkboxes);
-    
+
             if (checkallbox.hasAttribute('checked')) {
                 checkallbox.removeAttribute('checked');
-    
+
                 console.log('has attribute');
-                for (i = 0 ; i < checkboxes.length ; i ++ ) {
+                for (i = 0; i < checkboxes.length; i++) {
                     checkboxes[i].removeAttribute('checked');
-                    
+
                 }
             } else {
                 checkallbox.setAttribute('checked', '');
-    
+
                 console.log('doesnt haz attribute');
-                for (i = 0 ; i < checkboxes.length ; i ++ ) {
+                for (i = 0; i < checkboxes.length; i++) {
                     checkboxes[i].setAttribute('checked', '');
                 }
             }
-    
-    
-            
+
+
+
         })
     } catch (e) {
         console.log(e);
     }
-   
+
 
 }
