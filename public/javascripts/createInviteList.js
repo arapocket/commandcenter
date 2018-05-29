47ceb94831d2c042453ca96d69a334bdf12e74e2
arapocket <arakazaryan@me.com>
2018-05-29 13:05:47 -0700

    quick save

--- public/javascripts/createInviteList.js
+++ public/javascripts/createInviteList.js
@@ -20,31 +20,36 @@ function loadScripts() {
 
     var checkallbox = document.getElementById('checkallbox');
 
-    checkallbox.addEventListener('click', function(e){
-        console.log('checkallbox clicked');  
-        
-        console.log('logging checkboxes array');
-        console.log(checkboxes);
-
-        if (checkallbox.hasAttribute('checked')) {
-            checkallbox.removeAttribute('checked');
-
-            console.log('has attribute');
-            for (i = 0 ; i < checkboxes.length ; i ++ ) {
-                checkboxes[i].removeAttribute('checked');
-                
+    try {
+        checkallbox.addEventListener('click', function(e){
+            console.log('checkallbox clicked');  
+            
+            console.log('logging checkboxes array');
+            console.log(checkboxes);
+    
+            if (checkallbox.hasAttribute('checked')) {
+                checkallbox.removeAttribute('checked');
+    
+                console.log('has attribute');
+                for (i = 0 ; i < checkboxes.length ; i ++ ) {
+                    checkboxes[i].removeAttribute('checked');
+                    
+                }
+            } else {
+                checkallbox.setAttribute('checked', '');
+    
+                console.log('doesnt haz attribute');
+                for (i = 0 ; i < checkboxes.length ; i ++ ) {
+                    checkboxes[i].setAttribute('checked', '');
+                }
             }
-        } else {
-            checkallbox.setAttribute('checked', '');
-
-            console.log('doesnt haz attribute');
-            for (i = 0 ; i < checkboxes.length ; i ++ ) {
-                checkboxes[i].setAttribute('checked', '');
-            }
-        }
-
-
-        
-    })
+    
+    
+            
+        })
+    } catch (e) {
+        console.log(e);
+    }
+   
 
 }
