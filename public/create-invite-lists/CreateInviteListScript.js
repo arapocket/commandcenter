c67552d50353cf49501359fce2928c202ab511ff
arapocket <arakazaryan@me.com>
2018-06-21 14:36:02 -0700

    quick save

--- public/create-invite-lists/CreateInviteListScript.js
+++ public/create-invite-lists/CreateInviteListScript.js
@@ -3,7 +3,30 @@ function loadScripts() {
     var createButton = document.getElementById('createButton');
 
     createButton.addEventListener('click', function () {
-        console.log('createButton pressed');
+        postList();
+        console.log(dataArray);
     })
 
+
+
+
+
+    function postList() {
+        var xhr = new XMLHttpRequest();
+
+        if (!xhr) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+
+        xhr.open("POST", "http://ec2-34-215-115-69.us-west-2.compute.amazonaws.com:3000/createinvitelist", true);
+
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+
+        }));
+    }
+
 }
+
+
