1ccbc96a08118cbe66f89456341b188cd2044ec6
arapocket <arakazaryan@me.com>
2018-06-26 19:35:56 -0700

    quick save

--- public/create-invite-lists/InviteListCreatorScript.js
+++ public/create-invite-lists/InviteListCreatorScript.js
@@ -9,15 +9,28 @@ function loadScripts() {
 
     var input = document.getElementById('input');
 
+    var question = 0;
+
     function createID() {
         var newID = Math.random().toString(36).substr(2, 9);
         return newID;
     }
 
     function buttonPressed(){
-        jumbotronContent.innerText = "Which group is this invite list for?";
-        input.style.display = 'block';
-        button.innerText = 'Next'
+
+        if (question == 0){
+            jumbotronContent.innerText = "Which group is this invite list for?";
+            input.style.display = 'block';
+            button.innerText = 'Next'
+            question++;
+        } else if (question == 1){
+            jumbotronContent.innerText = "Dick?";
+            question++
+        } else if (question ==2){
+            jumbotronContent.innerText = "Jaj?";
+        }
+
+
     }
 
 }
