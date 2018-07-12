90ba23992e2516e3d26e2d96eb9112925b289b7c
arapocket <arakazaryan@me.com>
2018-07-12 10:12:21 -0700

    quick save

--- compute.js
+++ compute.js
@@ -1,25 +1,18 @@
-var compute = {};
 
+console.log('compute.js called');
 
-compute.process = function (){
-  console.log('compute.js called');
+    let sum = 0;
+    for (let i = 0; i < 1e9; i++) {
+      sum += i;
+    };
+    console.log('logging sum');
+    console.log(sum);
+  
+  if (process.send) {
+    process.send(streeng);
+  }
 
-  let sum = 0;
-  for (let i = 0; i < 1e8; i++) {
-    sum += i;
-  };
-  console.log('logging sum');
-  console.log(sum);
-
-if (process.send) {
-  process.send(sum);
-}
-
-
-process.on('message', message => {
-  console.log('message from parent:', message);
-});
-}
-
-module.exports = compute;
-
+  
+  process.on('message', message => {
+    console.log('message from parent:', message);
+  });
