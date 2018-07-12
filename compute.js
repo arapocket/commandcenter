e516508ab19d374f0b5fa5b42b480bddcc21d564
arapocket <arakazaryan@me.com>
2018-07-12 09:55:18 -0700

    quick save

--- compute.js
+++ compute.js
@@ -1,18 +1,15 @@
 
 console.log('compute.js called');
 
-const longComputation = () => {
     let sum = 0;
     for (let i = 0; i < 1e2; i++) {
       sum += i;
     };
     console.log('logging sum');
     console.log(sum);
-      return sum;
-  };
   
   if (process.send) {
-    process.send(longComputation);
+    process.send(sum);
   }
 
   
