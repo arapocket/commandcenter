44c72b46afce2056db471cd59c8350d5277c32ee
arapocket <arakazaryan@me.com>
2018-07-12 12:23:14 -0700

    quick save

--- compute.js
+++ compute.js
@@ -5,54 +5,46 @@ var sharp = require('sharp');
 
 var moveTo = "./public/photosforreader";
 
-console.log('compute.js called');
 
-    let sum = 0;
-    for (let i = 0; i < 1e9; i++) {
-      sum += i;
-    };
-    console.log('logging sum');
-    console.log(sum);
-  
-  if (process.send) {
-    process.send(sum);
-  }
+process.on('message', message => {
+  // console.log('message from parent:', message);
 
-  
-  process.on('message', message => {
-    // console.log('message from parent:', message);
-  
-    message.files.forEach(function (file, index) {
-      var fromPath = path.join(message.moveFrom, file);
-      var toPath = path.join(moveTo, file);
+  message.files.forEach(function (file, index) {
+    var fromPath = path.join(message.moveFrom, file);
+    var toPath = path.join(moveTo, file);
 
-      fs.stat(fromPath, function (error, stat) {
-        if (error) {
-          console.error("Error stating file.", error);
+    fs.stat(fromPath, function (error, stat) {
+      if (error) {
+        console.error("Error stating file.", error);
+        return;
+      }
+
+      if (stat.isFile()) {
+        // console.log( "'%s' is a file.", fromPath );
+      }
+
+      else if (stat.isDirectory()) {
+        // console.log( "'%s' is a directory.", fromPath );
+      }
+
+      // was 200, 300.  changed to smaller size 7/7/17  
+      sharp(fromPath).resize(100, 150).toFile(toPath, function (err) {
+        if (err) {
+          console.log("One of the files is not in expected format (.jpg) " + err);
           return;
         }
-
-        if (stat.isFile()) {
-          // console.log( "'%s' is a file.", fromPath );
-        }
-
-        else if (stat.isDirectory()) {
-          // console.log( "'%s' is a directory.", fromPath );
-        }
-
-        // was 200, 300.  changed to smaller size 7/7/17  
-        sharp(fromPath).resize(100, 150).toFile(toPath, function (err) {
-          if (err) {
-            console.log("One of the files is not in expected format (.jpg) " + err);
-            return;
-          }
-        });
-
       });
+
     });
-
-
-
-
-    
   });
+
+  if (process.send) {
+    process.send('done');
+  }
+
+
+
+
+
+
+});
