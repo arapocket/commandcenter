1bf333b49aa75f7413168aae52f73d0dc655d9f6
arapocket <arakazaryan@me.com>
2018-07-23 19:54:25 -0700

    quick save

--- app.js
+++ app.js
@@ -235,44 +235,69 @@ function getDevices(socket) {
 
   console.log('getDevices called');
 
-  https.get(process.env.SERVER_ADDRESS + '/guardnotifications', (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
 
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-        `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-        `Expected application/json but received ${contentType}`);
-    }
-    if (error) {
-      console.error(error.message);
-      // consume response data to free up memory
-      res.resume();
-      return;
-    }
+  var options = {
+    hostname: process.env.SERVER_ADDRESS,
+    port: 443,
+    path: '/guardnotifcations',
+    method: 'GET',
+    headers: {
+       }
+  };
 
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-
-        tokens = [];
-        for (var i = 0; i < parsedData.length; i++) {
-          tokens.push(parsedData[i].DeviceToken);
-        }
-        setSocketListeners(socket);
-
-      } catch (e) {
-        console.error(e.message);
-      }
+  var req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+  
+    res.on('data', (d) => {
+      process.stdout.write(d);
     });
-  }).on('error', (e) => {
   });
+  
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  
+  req.end();
+  
+  // https.get(process.env.SERVER_ADDRESS + '/guardnotifications', (res) => {
+  //   const { statusCode } = res;
+  //   const contentType = res.headers['content-type'];
+
+  //   let error;
+  //   if (statusCode !== 200) {
+  //     error = new Error('Request Failed.\n' +
+  //       `Status Code: ${statusCode}`);
+  //   } else if (!/^application\/json/.test(contentType)) {
+  //     error = new Error('Invalid content-type.\n' +
+  //       `Expected application/json but received ${contentType}`);
+  //   }
+  //   if (error) {
+  //     console.error(error.message);
+  //     // consume response data to free up memory
+  //     res.resume();
+  //     return;
+  //   }
+
+  //   res.setEncoding('utf8');
+  //   let rawData = '';
+  //   res.on('data', (chunk) => { rawData += chunk; });
+  //   res.on('end', () => {
+  //     try {
+  //       const parsedData = JSON.parse(rawData);
+
+  //       tokens = [];
+  //       for (var i = 0; i < parsedData.length; i++) {
+  //         tokens.push(parsedData[i].DeviceToken);
+  //       }
+  //       setSocketListeners(socket);
+
+  //     } catch (e) {
+  //       console.error(e.message);
+  //     }
+  //   });
+  // }).on('error', (e) => {
+  // });
 
 }
 
