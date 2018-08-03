57e619fa15f5bebc3d857ab3a530418e8b7b6e48
arapocket <arakazaryan@me.com>
2018-08-03 09:44:50 -0700

    quick save

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ if (process.env.CC_SSL == "YES") {
     console.log('App listening on port ' + port +  '!')
   });
 
-  server.listen(443);
+  server.listen(8443);
 
 } else {
   var server = app.listen(port, function () {
