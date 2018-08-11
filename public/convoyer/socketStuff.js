20ed9fea0cd16ed1014d9faa3d05b6607fdb9995
arapocket <arakazaryan@me.com>
2018-08-10 18:19:01 -0700

    quick save

--- public/convoyer/socketStuff.js
+++ public/convoyer/socketStuff.js
@@ -4,7 +4,7 @@ $(function () {
 
   getMessages();
 
-  var socket = io();
+  var socket = io('http://ec2-34-215-115-69.us-west-2.compute.amazonaws.com:3000');
 
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
